8f3f303bc07151de1ed9b262500dfc72a3d33f79
ashahid91 <94854691+ashahid91@users.noreply.github.com>
2022-01-11 13:00:09 -0500

    created object repository for logIn page

--- src/test/java/com/noorteck/qa/pages/LoginPage.java
+++ src/test/java/com/noorteck/qa/pages/LoginPage.java
@@ -1,7 +1,38 @@
 package com.noorteck.qa.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.noorteck.qa.utils.CommonUI;
+
+public class LoginPage extends CommonUI {
+	
+	@FindBy(id = "txtUsername")
+	WebElement userNameField;
+	
+	@FindBy(id = "txtPassword")
+	WebElement passwordField;
+	
+	@FindBy(xpath="//button[@type='submit']")
+	WebElement logInButton;
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void enterUserName(String userName) {
+		enter(userNameField, userName);
+	}
+	
+	public void enterPassword(String password) {
+		enter(passwordField, password);
+	}
+	
+	public void clickLogInButton() {
+		click(logInButton);
+	}
+	
 }
 
 
